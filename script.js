294e492c5c87d892cff68319afff94acde3b5ce2
Aryele Amanda <63553472+aryeleamanda23@users.noreply.github.com>
2020-05-11 19:40:44 -0300

    Adicion
    Adicionando arquivos js, css e HTML

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('JavaScript carregado');
+
+function validacao() {
+	console.log('Iniciando validacao CPF');
+	var cpf = document.getElementById('cpf_digitado').value
+	console.log(cpf);
+}
+
